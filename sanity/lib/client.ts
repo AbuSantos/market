3c61009d3a6f8e27b4352b8719360c8224d44dbe
Abusantos <abusomwansantos@gmail.com>
2023-12-18 19:57:34 +0100

    updating the checkoiut and stripe

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,6 +7,4 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token:
-    "skQOP0teZe6Fz42zPDuDseqg7qzXcEaRfmEp7Rj2Yh1EBSlGiuOi5Lk4if5r6hH2DhZo92qaxTTQixYuyqBFPfPPtdT7jCZIdu13dKybd4JIvLbGKSYnoEtCr0I4XiPFDDnJRsEbiVT4Jmk0ehe0vm0CjhVmmHuGRdGvRunr5jKfJ0xZiVPK",
 })
